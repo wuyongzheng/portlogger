1cb1d1e4ed1f1c3a657b350dd23a7344f98af647
Wu Yongzheng <wuyongzheng@gmail.com>
2014-07-22 00:18:10 +0800

    log recv data

--- portlogger.c
+++ portlogger.c
@@ -10,10 +10,27 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define RECV_TIMEOUT  120
+#define MAX_CLIENTNUM 100
+#define MAX_DATASIZE  128
+
+struct client_sock_struct {
+	int sock;
+	int local_port;
+	char remote_addr[INET6_ADDRSTRLEN];
+	int remote_port;
+	time_t conn_time;
+	unsigned char recvdata[MAX_DATASIZE];
+	int recvsize;
+	struct client_sock_struct *prev, *next;
+};
+
 static FILE *logfp;
 static int *listen_ports;
 static int *ssockets;
 static int nssocket;
+static struct client_sock_struct client_head; // head.next = oldest, head.prev = newest
+static int client_num = 0;
 
 static void change_to_nobody (void)
 {
@@ -30,28 +47,124 @@ static void change_to_nobody (void)
 	seteuid(nobody->pw_uid);
 }
 
+static struct client_sock_struct *new_client_sock (int sock)
+{
+	struct client_sock_struct *client_sock = malloc(sizeof(struct client_sock_struct));
+	client_sock->sock = sock;
+	client_sock->local_port = -1;
+	client_sock->remote_port = -1;
+	client_sock->remote_addr[0] = '\0';
+	client_sock->conn_time = time(NULL);
+	memset(client_sock->recvdata, 0, MAX_DATASIZE);
+	client_sock->recvsize = 0;
+	assert(client_head.prev->next == &client_head);
+	client_sock->prev = client_head.prev;
+	client_sock->next = &client_head;
+	client_head.prev->next = client_sock;
+	client_head.prev = client_sock;
+	client_num ++;
+	return client_sock;
+}
+
+static void log_and_delete_client_sock (struct client_sock_struct *client_sock)
+{
+	assert(client_num > 0);
+
+	if (client_sock->sock != -1) {
+		close(client_sock->sock);
+		client_sock->sock = -1;
+	}
+
+	char *timestr = ctime(&client_sock->conn_time);
+	if (timestr != NULL && timestr[0] != 0)
+		timestr[strlen(timestr) - 1] = '\0';
+	fprintf(logfp, "%s\t%d\t%s\t%d\t%d\t",
+			timestr, client_sock->local_port,
+			client_sock->remote_addr, client_sock->remote_port,
+			client_sock->recvsize);
+	int i;
+	for (i = 0; i < client_sock->recvsize; i ++) {
+		int c = client_sock->recvdata[i];
+		if (c == '\\')
+			fputs("\\\\", logfp);
+		else if (c >= ' ' && c <= '~')
+			fputc(c, logfp);
+		else if (c == '\r')
+			fputs("\\r", logfp);
+		else if (c == '\n')
+			fputs("\\t", logfp);
+		else if (c == '\t')
+			fputs("\\t", logfp);
+		else if (c == 0)
+			fputs("\\0", logfp);
+		else
+			fprintf(logfp, "\\x%02x", c);
+	}
+	fputs("\n", logfp);
+	fflush(logfp);
+
+	client_sock->prev->next = client_sock->next;
+	client_sock->next->prev = client_sock->prev;
+	client_num --;
+	free(client_sock);
+}
+
 static void server_loop (void)
 {
-	fd_set rfds;
-	int maxfdp1 = 0, i;
-
-	for (i = 0; i < nssocket; i ++)
-		if (maxfdp1 < ssockets[i])
-			maxfdp1 = ssockets[i];
-	maxfdp1 ++;
+	client_head.sock = -1;
+	client_head.conn_time = 0;
+	client_head.prev = client_head.next = &client_head;
 
 	while (1) {
+		fd_set rfds;
+		int maxfd = 0, i;
+
 		FD_ZERO(&rfds);
-		for (i = 0; i < nssocket; i ++)
+		for (i = 0; i < nssocket; i ++) {
 			FD_SET(ssockets[i], &rfds);
-		int retval = select(maxfdp1, &rfds, NULL, NULL, NULL);
+			maxfd = ssockets[i] > maxfd ? ssockets[i] : maxfd;
+		}
+		struct client_sock_struct *client_sock;
+		for (client_sock = client_head.next;
+				client_sock != &client_head;
+				client_sock = client_sock->next) {
+			FD_SET(client_sock->sock, &rfds);
+			maxfd = client_sock->sock > maxfd ? client_sock->sock : maxfd;
+		}
+		struct timeval timeout;
+		timeout.tv_sec = RECV_TIMEOUT;
+		timeout.tv_usec = 0;
+		int retval = select(maxfd + 1, &rfds, NULL, NULL, &timeout);
 
 		if (retval == -1) {
 			fprintf(logfp, "select() error\n");
 			return;
 		}
-		if (retval == 0) // WTF
+
+		if (retval == 0) { // dump all client socks. there should be only one though
+			while (client_num > 0)
+				log_and_delete_client_sock(client_head.next);
 			continue;
+		}
+
+		/* do client sockets first. */
+		for (client_sock = client_head.next;
+				client_sock != &client_head;
+				client_sock = client_sock->next) {
+			if (!FD_ISSET(client_sock->sock, &rfds))
+				continue;
+			assert(client_sock->recvsize < MAX_DATASIZE);
+			ssize_t recvsize = recv(client_sock->sock,
+					client_sock->recvdata + client_sock->recvsize,
+					MAX_DATASIZE - client_sock->recvsize, MSG_DONTWAIT);
+			if (recvsize > 0 && client_sock->recvsize + recvsize < MAX_DATASIZE) {
+				client_sock->recvsize += recvsize;
+				continue;
+			}
+			log_and_delete_client_sock(client_sock);
+		}
+
+		/* do server sockets next */
 		for (i = 0; i < nssocket; i ++) {
 			if (!FD_ISSET(ssockets[i], &rfds))
 				continue;
@@ -62,19 +175,18 @@ static void server_loop (void)
 				fprintf(logfp, "Warning: accept() error\n");
 				continue;
 			}
-			close(sock);
 
-			char addrstr [64];
-			const char *tmp = inet_ntop(cli_addr.sin_family, &(cli_addr.sin_addr), addrstr, sizeof(addrstr));
+			client_sock = new_client_sock(sock);
+			client_sock->local_port = listen_ports[i];
+			client_sock->remote_port = ntohs(cli_addr.sin_port);
+			const char *tmp = inet_ntop(cli_addr.sin_family,
+					&(cli_addr.sin_addr),
+					client_sock->remote_addr,
+					sizeof(client_sock->remote_addr));
 			assert(tmp != NULL);
-			time_t ltime = time(NULL);
-			char *timestr = ctime(&ltime);
-			if (timestr != NULL && timestr[0] != 0)
-				timestr[strlen(timestr) - 1] = '\0';
-			fprintf(logfp, "%s\t%d\t%s\t%d\n",
-					timestr, listen_ports[i],
-					addrstr, ntohs(cli_addr.sin_port));
-			fflush(logfp);
+
+			while (client_num > MAX_CLIENTNUM)
+				log_and_delete_client_sock(client_head.next);
 		}
 	}
 }
@@ -124,9 +236,22 @@ int main (int argc, char *argv[])
 		}
 	}
 
+	{
+		time_t ts = time(NULL);
+		char *timestr = ctime(&ts);
+		if (timestr != NULL && timestr[0] != 0)
+			timestr[strlen(timestr) - 1] = '\0';
+		fprintf(logfp, "%s\tListening to: ", timestr);
+		for (i = 0; i < nssocket; i ++)
+			fprintf(logfp, " %d", listen_ports[i]);
+		fprintf(logfp, "\n");
+		fflush(logfp);
+	}
+
 	fclose(stdin);
 	fclose(stdout);
 	fclose(stderr);
+	chdir("/");
 	change_to_nobody();
 	if (fork() != 0) return 0;
 	server_loop();
